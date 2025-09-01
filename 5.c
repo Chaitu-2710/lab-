dee7ada751bfa86b139f956ce720a0c6b8da8fb5
chaitanya <chaitanya.oruganti.1234@gmail.com>
2025-09-01 20:06:38 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -61,3 +61,53 @@ rt[i].from[j]=j;
 do
 
 {
+count=0;
+
+for(i=0;i<nodes;i++)//We choose arbitary vertex k and we calculate the direct distance
+
+from the node i to k using the cost matrix
+
+//and add the distance from k to node j
+
+for(j=0;j<nodes;j++)
+
+for(k=0;k<nodes;k++)
+
+if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
+
+{//We calculate the minimum distance
+
+rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
+
+rt[i].from[j]=k;
+
+Dept of CSE
+
+Γ
+
+
+count++;
+
+}
+
+}while(count!=0); for(i=0;i<nodes;i++)
+
+{
+
+printf("\n\n For router %d\n",i+1);
+
+for(j=0;j<nodes;j++)
+
+{
+
+printf("\t\nnode %d via %d Distance %d ",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+
+}
+
+}
+
+printf("\n\n");
+
+getch();
+
+}
