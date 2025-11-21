4c0edaeea6957966c58f45484d0586e4fea07d2e
chaitanya <chaitanya.oruganti.1234@gmail.com>
2025-11-21 12:10:27 +0530

    Update 5.py

--- 2.py
+++ 2.py
@@ -1,60 +1,43 @@
 #include <stdio.h>
 
+struct node {
+    unsigned dist[20];
+    unsigned from[20];
+} rt[10];
+
 int main() {
-    int data[50], div[20], rem[20];
-    int datalen, divlen;
-    int i, j, k, ch;
+    int cost[20][20], nodes, i, j, k, updated;
 
-    printf("Enter the data (binary): ");
-    i = 0;
-    while ((ch = getchar()) != '\n') {
-        data[i++] = (ch == '1') ? 1 : 0;
-    }
-    datalen = i;
+    printf("Enter number of nodes: ");
+    scanf("%d", &nodes);
 
-    printf("Enter the divisor (binary): ");
-    i = 0;
-    while ((ch = getchar()) != '\n') {
-        div[i++] = (ch == '1') ? 1 : 0;
-    }
-    divlen = i;
-
-    // Append zeros
-    for (i = datalen; i < datalen + divlen - 1; i++)
-        data[i] = 0;
-
-    int total_len = datalen + divlen - 1;
-
-    // Copy first divisor length bits into remainder
-    for (i = 0; i < divlen; i++)
-        rem[i] = data[i];
-
-    k = divlen - 1;
-
-    while (k < total_len) {
-        if (rem[0] == 1) {
-            for (i = 0; i < divlen; i++)
-                rem[i] = rem[i] ^ div[i];
+    printf("Enter cost matrix:\n");
+    for (i = 0; i < nodes; i++) {
+        for (j = 0; j < nodes; j++) {
+            scanf("%d", &cost[i][j]);
+            cost[i][i] = 0;
+            rt[i].dist[j] = cost[i][j];
+            rt[i].from[j] = j;
         }
-
-        // Shift Left
-        if (k == total_len - 1) break;
-
-        for (i = 0; i < divlen - 1; i++)
-            rem[i] = rem[i + 1];
-
-        rem[i] = data[++k];
     }
 
-    // Store remainder back into data
-    j = 1;
-    for (i = total_len - (divlen - 1); i < total_len; i++)
-        data[i] = rem[j++];
+    do {
+        updated = 0;
+        for (i = 0; i < nodes; i++)
+            for (j = 0; j < nodes; j++)
+                for (k = 0; k < nodes; k++)
+                    if (rt[i].dist[j] > cost[i][k] + rt[k].dist[j]) {
+                        rt[i].dist[j] = cost[i][k] + rt[k].dist[j];
+                        rt[i].from[j] = k;
+                        updated = 1;
+                    }
+    } while (updated);
 
-    printf("\nFinal data to be sent (CRC encoded): ");
-    for (i = 0; i < total_len; i++)
-        printf("%d", data[i]);
+    for (i = 0; i < nodes; i++) {
+        printf("\nRouter %d:\n", i + 1);
+        for (j = 0; j < nodes; j++)
+            printf("Node %d via %d distance %d\n", j + 1, rt[i].from[j] + 1, rt[i].dist[j]);
+    }
 
-    printf("\n");
     return 0;
 }
